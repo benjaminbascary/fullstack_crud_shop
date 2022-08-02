5e980452135a5ca0c11e5d9064d2f56a355a3d1c
Benjamin Bascary <99748547+benjaminbascary@users.noreply.github.com>
2022-08-01 21:42:03 -0300

    Adds a user model to the project. Deletes dummy databases

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../utils/mysql');
+
+// we define a model
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
